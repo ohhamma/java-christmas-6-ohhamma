c09e2664e00ef6399ef90301e80906c301791716
ohhamma <judyham10634@naver.com>
2023-11-15 15:38:00 +0900

    :test_tube: test(DateTest): Date 클래스에 대한 테스트케이스 구현
    방문 날짜가 유효한 경우에 대한 확인

--- src/test/java/christmas/domain/visit/DateTest.java
+++ src/test/java/christmas/domain/visit/DateTest.java
@@ -5,15 +5,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DateTest {
-    @DisplayName("방문 날짜가 1 이상 31 이하의 숫자 아닌 경우에 대한 예외 처리")
+    @DisplayName("방문 날짜가 1 이상 31 이하의 숫자가 아닌 경우에 대한 예외 처리")
     @ValueSource(ints = {-100, -1, 0, 32, 100})
     @ParameterizedTest
-    void dateOutOfRange(int date) {
-        assertThatThrownBy(() -> Date.from(date))
+    void dateOutOfRange(int day) {
+        assertThatThrownBy(() -> Date.from(day))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.INVALID_DATE.getMessage());
+    }
+
+    @DisplayName("방문 날짜가 유효한 경우에 대한 확인")
+    @ValueSource(ints = {1, 15, 31})
+    @ParameterizedTest
+    void dateIsValid(int day) {
+        Date date = Date.from(day);
+        LocalDate localDate = LocalDate.of(2023, 12, day);
+
+        assertTrue(date.isEqual(localDate));
     }
 }
